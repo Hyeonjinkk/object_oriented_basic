f64ae24e8e85065cace10178228b5aebf4369e84
Hyeonjinkk <guswls0975@naver.com>
2022-07-12 20:45:40 +0900

    Description : static private하게

--- Object_Oriented_Basic/src/ch16/Employee.java
+++ Object_Oriented_Basic/src/ch16/Employee.java
@@ -2,7 +2,7 @@ package ch16;
 
 public class Employee {
 
-	public static int serialNum = 1000;
+	private static int serialNum = 1000;
 	
 	private int employeeId;
 	private String employeeName;
@@ -18,24 +18,34 @@ public class Employee {
 	}	// end of constructor
 	
 	// Generate getter and setter
+	
+	public static int getSerialNum() {
+		
+		return serialNum;
+	}
+	
 	public int getEmployeeId() {
 		return employeeId;
 	}
 	public void setEmployeeId(int employeeId) {
 		this.employeeId = employeeId;
 	}
+	
 	public String getEmployeeName() {
 		return employeeName;
 	}
 	public void setEmployeeName(String employeeName) {
 		this.employeeName = employeeName;
 	}
+	
 	public String getDepartment() {
 		return department;
 	}
 	public void setDepartment(String department) {
 		this.department = department;
 	}
+
+	
 	
 	
 }
--- Object_Oriented_Basic/src/ch16/EmployeeTest.java
+++ Object_Oriented_Basic/src/ch16/EmployeeTest.java
@@ -3,10 +3,11 @@ package ch16;
 public class EmployeeTest {
 
 	public static void main(String[] args) {
+		// private 하게
+		System.out.println("기준 사번은 " + Employee.getSerialNum() + "입니다.\n" + "==================================");
 		
 		var employeeLee = new Employee();
 		employeeLee.setEmployeeName("이순신");
-		
 		
 		
 		var employeeKim = new Employee();
