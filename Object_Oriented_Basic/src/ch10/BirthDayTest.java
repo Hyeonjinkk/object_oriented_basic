cf62d3c837c9f025e4eae42d13d10feca0b37c0f
Hyeonjinkk <guswls0975@naver.com>
2022-07-12 18:49:51 +0900

    Create package ch10 List --------------------------- BirthDay.java BirthDayTest.java

---
+++ Object_Oriented_Basic/src/ch10/BirthDayTest.java
@@ -0,0 +1,15 @@
+package ch10;
+
+public class BirthDayTest {
+	
+	public static void main(String[] args) {
+
+		var date = new BirthDay();
+		date.setYear(1997);
+		date.setMonth(12);
+		date.setDay(28);
+		
+		date.showDate();
+		
+	}
+}
